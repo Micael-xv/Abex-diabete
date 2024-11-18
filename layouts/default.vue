681461b9c74bc4898db08da488b95f4e890e7d9b
Micael <micaelxv1@gmail.com>
2024-11-18 20:56:40 -0300

    forget

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,9 +8,9 @@
       >
         <v-list>
           <v-list-item
-            prepend-avatar="https://avatars.githubusercontent.com/u/75547915?v=4"
-            subtitle="micael.trevisan@unochapeco.edu.br"
-            title="Micael Trevisan"
+            prepend-avatar="algo"
+            subtitle="algo"
+            title="algo"
           />
         </v-list>
 
@@ -18,10 +18,6 @@
 
         <v-list density="compact" nav>
           <v-list-item prepend-icon="mdi-home" to="/" title="Home" value="home"/>
-          <v-list-item prepend-icon="mdi-table" to="/tabela" title="Tabela elementos" value="Tabela"/>
-          <v-list-item prepend-icon="mdi-twitter" to="/x" title="Twitter" value="starred"/>
-          <v-list-item prepend-icon="mdi-bug-check-outline" to="/funcao" title="Funcao for" value="funcao"/>
-
         </v-list>
       </v-navigation-drawer>
       <v-main style="height: 100vh; background-color: black">
--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,8 @@
 <template>
-  <v-container class="pa-4 text-center">
-    <h1 style="color: white">{{ valor }}</h1>
-    <BotaoPadraoComponent
-      text="Contar"
-      :dialog="dialog"
-      @clicou-botao="abrirDialog"
-      @excluir="deleteItem"
-    />
-    <v-btn class="ml-2" color="pink" @click="mudaPagina()">
-      <v-icon> mdi-twitter </v-icon>
-    </v-btn>
-
-    <v-btn class="ml-2" color="green" @click="funcFor">
-      <v-icon> mdi-bug-check-outline </v-icon>
-    </v-btn>
+  <v-container>
+    <h1 style="color: white">Diabetes</h1>
   </v-container>
 </template>
 
 <script>
-export default {
-  data: () => {
-    return {
-      ativo: false,
-      dialog: false,
-      valor: 0,
-    };
-  },
-
-  watch: {
-    dialog(evento) {
-      this.valor++;
-    },
-  },
-
-  created() {},
-
-  methods: {
-    async getItens() {
-      const response = await this.$api.get("/elemento");
-      this.items = response.data;
-      this.loading = false;
-    },
-    async deleteItem(items) {
-      if (confirm(`Deseja deletar o registro com id ${items.id}`)) {
-        const response = await this.$api.post("/elemento/destroy", {
-          id: items.id,
-        });
-        if (response.type == "error") {
-          alert(response.message);
-        }
-      }
-      await this.getItens();
-    },
-    async persist() {
-      if (this.atividade.id) {
-        const response = await this.$api.post(
-          `/elemento/persist/${this.atividade.id}`,
-          this.atividade
-        );
-      } else {
-        const response = await this.$api.post(
-          "/elemento/persist",
-          this.atividade
-        );
-      }
-      this.resetAtividade();
-      await this.getItems();
-    },
-    mudaPagina() {
-      this.$router.push({ path: "/x" });
-    },
-    funcFor() {
-      this.$router.push({ path: "/funcao" });
-    },
-    abrirDialog() {
-      this.dialog = true;
-      this.valor++;
-    },
-  },
-};
 </script>
