ba99c62f53cd9fed01feb5f9d3a0fab24e3fcaf2
Julia <juteston@unochapeco.edu.br>
2024-11-25 15:19:09 -0300

    adc pagina

---
+++ pages/page-inicio.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Bem-vindo, usuário!</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <button class="btn btn-primary">Análise</button>
+        </div>
+        <div class="col-md-4">
+          <button class="btn btn-secondary">Histórico de análises</button>
+        </div>
+        <div class="col-md-4">
+          <button class="btn btn-danger">Logout</button>
+        </div>
+      </div>
+    </div>
+  </template>
