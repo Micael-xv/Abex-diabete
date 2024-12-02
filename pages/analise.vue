dca7f341c6983c8f05414dd4f76d1dfba9b0b6ae
Micael <micaelxv1@gmail.com>
2024-12-02 01:52:57 -0300

    forget

--- pages/analise.vue
+++ pages/analise.vue
@@ -139,7 +139,7 @@
             </v-row>
             <v-row>
               <v-col>
-                <p>Gravidezes: O número de vezes que a paciente esteve grávida. A gravidez pode afetar a sensibilidade à insulina e um número maior de gestações pode indicar um risco maior de desenvolver diabetes.</p>
+                <p>Gravidez: O número de vezes que a paciente esteve grávida. A gravidez pode afetar a sensibilidade à insulina e um número maior de gestações pode indicar um risco maior de desenvolver diabetes.</p>
               </v-col>
             </v-row>
             <v-row>
@@ -176,6 +176,23 @@
         </v-card>
       </v-col>
     </v-row>
+
+    <v-dialog v-model="showDialog" rounded="xl" max-width="600">
+      <v-card>
+        <v-card-title class="text-h6 d-flex justify-center">
+          Cuidados com a diabetes
+        </v-card-title>
+        <v-card-text>
+          <p>Escolhas saudáveis: O paciente portador de diabetes deve fazer escolhas saudáveis para sua alimentação. Opte por alimentos mais naturais como vegetais, frutas, sementes e grãos. Evitando assim consumir com frequência alimentos processados e refinados que aumentam a glicemia.</p>
+          <p>Cuide com o açúcar: O excesso de ingestão de açúcar pode desregular a glicemia, resultando em possíveis complicações da doença. Evite alimentos, como sobremesas, feitos com açúcar refinado. Na vontade de um docinho, escolha uma fruta da estação, elas são mais doces e de fácil acesso (lembrando que é uma porção por vez).</p>
+          <p>Consumo de frutas: As frutas podem e devem ser consumidas mesmo por quem tem diabetes, mas a recomendação é uma por vez, podendo comer de 3 a 5 por dia (qualquer fruta da sua preferência).</p>
+          <p>Se alimente regularmente: Sugere-se que a pessoa com diabetes não deve permanecer muito tempo sem se alimentar. Respeite os sinais do seu organismo e faça refeições em intervalos regulares. Isso evita picos de hipoglicemia e hiperglicemia.</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" text @click="showDialog = false">Fechar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
@@ -201,11 +218,25 @@ const { $aiApi } = useNuxtApp();
 
 const sendPredictionRequest = async () => {
   try {
+    if (!form.value.Age || !form.value.Pregnancies || !form.value.Glucose || !form.value.BloodPressure || !form.value.SkinThickness || !form.value.Insulin || !form.value.BMI || !form.value.DPF) {
+      result.value = "Preencha todos os campos.";
+      return;
+    }
     const response = await $aiApi.post("http://127.0.0.1:8000/predict", form.value);
     result.value = `Previsão: ${response.prediction === 1 ? "Diabetes" : "Sem Diabetes"}`;
   } catch (error) {
     console.error("Erro na previsão:", error);
     result.value = "Erro ao obter previsão.";
   }
+};
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      showDialog: false,
+    };
+  },
 };
 </script>
